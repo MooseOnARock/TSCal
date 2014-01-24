118674b37d2bdd3c53bfe1f0955906bb1d229f5d
Tyler Sanborn <tysanborn@gmail.com>
2014-01-24 14:26:09 -0500

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+	var calendar = jQuery('#calendar');
+	var days = calendar.find('.day');
+	var curDate = new Date();
+	var curMonth = curDate.getMonth();
+	curDate.setMonth(curMonth,1);
+	console.log(curDate);
+	for(var i = curDate.getDay(); curDate.getMonth() == curMonth; i++){
+		days.eq(i).text(curDate.getDate());
+		curDate.setDate(curDate.getDate()+1);
+	}
+});
