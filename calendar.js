d7dd69d86e1a5d687c0fedeadf252541b082d20b
Tyler Sanborn <tysanborn@gmail.com>
2014-01-24 16:08:18 -0500

    Update calendar.js

--- calendar.js
+++ calendar.js
@@ -2,12 +2,14 @@ jQuery(document).ready(function(){
 	var calendar = jQuery('#calendar');
 	var days = calendar.find('.day');
 	var curDate = new Date();
+	displayDays(curDate.getMonth());
+});
+function displayDays(curMonth){
 	var curMonth = curDate.getMonth();
 	curDate.setMonth(curMonth,1);
-	console.log(curDate);
 	for(var i = curDate.getDay(); curDate.getMonth() == curMonth; i++){
 		days.eq(i).text(curDate.getDate());
 		curDate.setDate(curDate.getDate()+1);
 	}
-});
+}
 window.months = ['January', 'Feburary', 'March', 'April'];
